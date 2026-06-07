52a233a4d71502083c78cd5994816582bc68cc3c
DeepakVerma <deepakverma8647@gmail.com>
2026-06-07 16:18:34 +0530

    fix: use environment variables for config

--- Server/config/server.config.js
+++ Server/config/server.config.js
@@ -1,9 +1,7 @@
-export const server={
-PORT:8080,
-DB_URL:"mongodb+srv://Deepak:EvNFJ75uJ1FDw2JS@safar.z0ghpnf.mongodb.net/?retryWrites=true&w=majority&appName=Safar",
-Secret_code:'This is vary secrate',
-Gmailuser: "safarofficial4049@gmail.com",
-Gmailpass: "szrv xnzt ulwj qske"
-
-
+export const server = {
+  PORT: process.env.PORT || 8080,
+  DB_URL: process.env.MONGO_URI || "mongodb+srv://Deepak:EvNFJ75uJ1FDw2JS@safar.z0ghpnf.mongodb.net/?retryWrites=true&w=majority&appName=Safar",
+  Secret_code: process.env.SECRET_CODE || 'This is vary secrate',
+  Gmailuser: process.env.GMAIL_USER || "safarofficial4049@gmail.com",
+  Gmailpass: process.env.GMAIL_PASS || "szrv xnzt ulwj qske"
 }
